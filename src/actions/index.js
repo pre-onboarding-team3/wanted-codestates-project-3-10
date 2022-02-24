432e165b807b8be61be000863dbbf0d91b5a53fc
krim45 <krim45@naver.com>
2022-02-24 18:14:47 +0900

    :sparkles:feat: keyDown Action 추가

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,20 @@
 export const SEARCH = 'SEARCH';
+export const KEY_DOWN = 'KEY_DOWN';
 
 export const search = items => {
   return {
     type: SEARCH,
     payload: {
       items,
+    },
+  };
+};
+
+export const keyDonw = keyword => {
+  return {
+    type: KEY_DOWN,
+    payload: {
+      keyword,
     },
   };
 };
